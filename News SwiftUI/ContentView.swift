859ea2e991698c910969547147d4c906bea497a7
maximsamus <maximsamus1@gmail.com>
2022-08-16 14:22:50 +0300

    Initial Commit

---
+++ News SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News SwiftUI
+//
+//  Created by Максим Самусь on 16.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News SwiftUI/News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SwiftUIApp.swift
+//  News SwiftUI
+//
+//  Created by Максим Самусь on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
